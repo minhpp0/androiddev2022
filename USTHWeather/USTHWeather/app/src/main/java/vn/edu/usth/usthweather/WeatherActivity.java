4b80ba53b08f4694e79e61f43fa588d49c8b14e5
minhpp0 <minhnbn.ba9042@st.usth.edu.vn>
2021-10-20 11:50:48 +0700

    Submit lw8

--- USTHWeather/USTHWeather/app/src/main/java/vn/edu/usth/usthweather/WeatherActivity.java
+++ USTHWeather/USTHWeather/app/src/main/java/vn/edu/usth/usthweather/WeatherActivity.java
@@ -1,9 +1,12 @@
 package vn.edu.usth.usthweather;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
 
 import android.os.Bundle;
 import android.util.Log;
@@ -11,11 +14,17 @@ import android.util.Log;
 public class WeatherActivity extends AppCompatActivity {
     private static final String STATE = "STATUS";
 
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
         Log.i(STATE, "onCreate");
+        viewPager = findViewById(R.id.viewpager);
+        tabLayout.setupWithViewPager(viewPager);
+        tabLayout = findViewById(R.id.viewpager);
     }
     @Override
     protected void onStart() {
@@ -50,5 +59,6 @@ public class WeatherActivity extends AppCompatActivity {
 
         Log.i(STATE, "onDestroy");
     }
+
 
 }
